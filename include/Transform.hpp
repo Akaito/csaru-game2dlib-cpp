7d2eddfb9e353e119c406bfe1d6750f79092e5bb
Chris Barrett <chris@codesaru.com>
2016-04-18 00:16:52 -0700

    Add Transform and _UseVendor(CML).

---
+++ include/Transform.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <cml/cml.h> // Configurable Math Library
+
+namespace CSaru2d {
+
+class Transform {
+
+private: // Data
+
+	cml::vector3f m_position;
+    float         m_rotation;
+	cml::vector3f m_scale;
+    
+	cml::vector3f m_velocity; // Hacks! (?)
+
+public:
+    // Construction
+    Transform();
+    virtual ~Transform();
+    
+    // Queries
+    void                  GetWorldFromModelMtx (cml::matrix44f_c * worldMatrixOut) const;
+    const cml::vector3f & GetPosition () const     { return m_position; }
+    float                 GetRotation () const     { return m_rotation; }
+    const cml::vector3f & GetScale () const        { return m_scale;    }
+    const cml::vector3f & GetVelocity () const     { return m_velocity; }
+
+    // Commands
+    void SetPosition (const cml::vector3f & position);
+    void SetRotation (float rotation);
+    void SetScale (const cml::vector3f & scale);
+    
+    void SetVelocity (const cml::vector3f & velocity);
+    void SetVelocity (float x, float y);
+
+};
+
+} // namespace CSaru2d
+
---
+++ src/Transform.cpp
@@ -0,0 +1,60 @@
+#include "../include/Transform.hpp"
+
+namespace CSaru2d {
+
+//==============================================================================
+Transform::Transform () : m_rotation(0.0f) {
+
+  m_position.zero();
+  m_scale.set(1.0f, 1.0f, 1.0f);
+  m_velocity.zero();
+
+}
+
+//==============================================================================
+Transform::~Transform () {
+}
+
+//==============================================================================
+void Transform::GetWorldFromModelMtx (cml::matrix44f_c * worldMatrixOut) const {
+
+    cml::matrix44f_c translation;
+	cml::matrix44f_c rotationZ;
+	cml::matrix44f_c scale;
+
+	cml::matrix_translation(translation, m_position[0], m_position[1], 0.0f);
+	cml::matrix_rotation_world_z(rotationZ, m_rotation);
+	cml::matrix_scale(scale, m_scale[0], m_scale[1], 1.0f);
+
+    //*worldMatrixOut = translation * rotationZ * scale;
+    *worldMatrixOut = scale * rotationZ * translation;
+
+}
+
+//==============================================================================
+void Transform::SetPosition (const cml::vector3f & position) {
+    m_position = position;
+}
+
+//==============================================================================
+void Transform::SetRotation (float rotation) {
+    m_rotation = rotation;
+}
+
+//==============================================================================
+void Transform::SetScale (const cml::vector3f & scale) {
+    m_scale = scale;
+}
+
+//==============================================================================
+void Transform::SetVelocity (const cml::vector3f & velocity) {
+    m_velocity = velocity;
+}
+
+//==============================================================================
+void Transform::SetVelocity (float x, float y) {
+    m_velocity.set(x, y, 0.0f);
+}
+
+} // namespace CSaru2d
+
